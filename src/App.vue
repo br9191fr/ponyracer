2f5ba1bc5fc4adf0ba8d4b691c06f6f6b4c3e499
Bruno Ricci <br9191fr@gmail.com>
2022-10-31 09:59:48 +0100

    First commit for Pony Races

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="container" style="margin-top: 70px">
+    <h1>Ponyracer</h1>
+    <Races></Races>
+
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import Races from "@/Races.vue";
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const numberOfUsers = ref(146);
+    // update the counter after 3 seconds
+    // but the template does not reflect the new value
+    setTimeout(() => (numberOfUsers.value = numberOfUsers.value+ 1), 3000);
+    return {numberOfUsers};
+  },
+  components: {
+    Races}
+});
+</script>
+
+<style></style>
---
+++ src/Races.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Races</h2>
+  </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: "Races"
+});
+</script>
+
+<style scoped>
+
+</style>

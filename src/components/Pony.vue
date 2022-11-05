ae491f979e6982886213d2f46297be9d3ab20dfa
Bruno Ricci <br9191fr@gmail.com>
2022-11-05 15:04:45 +0100

    Step 6 validated

---
+++ src/components/Pony.vue
@@ -0,0 +1,41 @@
+<template>
+  <figure @click="clicked()">
+    <img :src="ponyImageUrl" :alt="ponyModel.name" />
+    <figcaption>{{ ponyModel.name }}</figcaption>
+  </figure>
+</template>
+<script lang="ts">
+import { computed, defineComponent, PropType } from 'vue';
+import { PonyModel } from '@/models/PonyModel';
+export default defineComponent({
+  name: 'Pony',
+  props: {
+    ponyModel: {
+      type: Object as PropType<PonyModel>,
+      required: true
+    }
+  },
+  emits: {
+    ponySelected: () => true
+  },
+  setup(props, { emit }) {
+    const ponyImageUrl = computed(() => `/images/pony-${props.ponyModel.color.toLowerCase()}.gif`);
+    function clicked() {
+      emit('ponySelected');
+    }
+    return { ponyImageUrl, clicked };
+  }
+});
+</script>
+
+<style scoped>
+figure {
+  margin: 3px;
+  padding: 3px;
+  font-size: 12px;
+}
+
+img {
+  height: 50px;
+}
+</style>
--- src/components/Race.vue
+++ src/components/Race.vue
@@ -2,19 +2,21 @@
   <div>
     <h2>{{ raceModel.name }}</h2>
     <p>{{ startInstant }}</p>
-    <ul>
-      <li v-for="pony of raceModel.ponies" :key="pony.id">{{ pony.name }}</li>
-    </ul>
+    <div class="col" v-for="pony in raceModel.ponies" :key="pony.id">
+      <Pony :ponyModel="pony" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, PropType } from 'vue';
 import { RaceModel } from '@/models/RaceModel';
+import Pony from '@/components/Pony.vue';
 import fromNow from '@/utils/FromNow';
 
 export default defineComponent({
   name: 'Race',
+  components: { Pony },
   props: {
     raceModel: {
       type: Object as PropType<RaceModel>,
